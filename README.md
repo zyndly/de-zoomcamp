c6f4dc5f019edfc632fbd5b00ac09a8ace33feca
zyndly <zyndlykentperlinbaja@gmail.com>
2023-02-07 00:21:13 +0800

    add flows in week 2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Data Engineering Zoomcamp 2023 By [DataTalksClub](https://www.linkedin.com/company/datatalks-club/)
+
+Course Repository: [DataTalksClub/data-engineering-zoomcamp](https://github.com/DataTalksClub/data-engineering-zoomcamp) 
+<p align="center"> Personal Notes and Activities</p>
+
+<img src="images/de-zoomcamp-2023.png"/>
---
+++ week_2_workflow_orchestration/flows/etl_web_to_gcs.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read data from web into pandas DataFrame"""
+    #if randint(0, 1) > 0:
+    #    raise Exception
+    df = pd.read_csv(dataset_url)
+    print(df.head())
+    print(df.info())
+    return df
+
+@task(log_prints=True)
+def clean(df = pd.DataFrame) -> pd.DataFrame:
+    """fix dtype issues"""
+    #Green Taxi
+    #df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    #df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+
+    #Yellow Taxi
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    # delete "ehail_fee", only None entries
+    #df.drop(labels=['ehail_fee'], axis=1, inplace=True)
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """write DataFrame out as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    path_gcp = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path, path_gcp
+
+@task()
+def write_gcs(path: Path, path_gcp: Path) -> None:
+    """Upload local parquet file to Google Cloud Storage"""
+    gcs_block = GcsBucket.load("de-zoomcamp")
+    gcs_block.upload_from_path(
+        from_path=path,
+        to_path=path_gcp
+    )
+
+@flow()
+def etl_web_to_gcs() -> None:
+    """The main ETL function"""
+    color = "yellow"
+    year = 2019
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+    
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path, path_gcp = write_local(df_clean, color, dataset_file)
+    write_gcs(path, path_gcp)
+
+if __name__ == '__main__':
+    etl_web_to_gcs()
